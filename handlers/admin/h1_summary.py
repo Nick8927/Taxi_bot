2ed816480e4ef6975b6cf84d3b8de93c622387f7
Nick <idainswan@gmail.com>
2025-08-08 11:41:58 +0300

    модуль h1_summary исправлен, теперь есть четкая градация для отчета админу по определенным периодам

--- handlers/admin/h1_summary.py
+++ handlers/admin/h1_summary.py
@@ -1,29 +1,48 @@
 from aiogram import Router, F
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 
-from services.google_sheets import get_full_report
+from keyboards.reply import reply_admin_report_menu, reply_admin_menu
+from services.google_sheets import get_admin_summary
 
 router = Router()
 
 @router.message(F.text == "📊 Сводный отчёт")
-async def handle_summary_report(message: Message):
-    """отчёт для администратора"""
-    report = get_full_report()
-
-    total = report["total"]
-    text = (
-        f"📊 Сводный отчёт за всё время:\n"
-        f"Доход: {total['income']:.2f} ₽\n"
-        f"Расход: {total['expense']:.2f} ₽\n"
-        f"Разница: {total['diff']:.2f} ₽\n\n"
-        f"👤 По пользователям:\n"
+async def admin_summary_menu(message: Message, state: FSMContext):
+    """выбор периода для сводного отчёта"""
+    await state.clear()
+    await message.answer(
+        "Выберите период для сводного отчёта:",
+        reply_markup=reply_admin_report_menu()
     )
 
-    for username, stats in report["users"].items():
-        text += (
-            f"{username} — Доход: {stats['income']:.2f} ₽\n "
-            f"Расход: {stats['expense']:.2f} ₽\n "
-            f"Разница: {stats['diff']:.2f} ₽\n"
-        )
 
-    await message.answer(text)
+@router.message(F.text == "📅 Сегодня")
+async def admin_summary_today(message: Message):
+    """отчёт за сегодня"""
+    report = get_admin_summary("day")
+    await message.answer(f"📅 Сводный отчёт за сегодня:\n{report}")
+
+
+@router.message(F.text == "🗓 Этот месяц")
+async def admin_summary_month(message: Message):
+    """отчёт за месяц"""
+    report = get_admin_summary("month")
+    await message.answer(f"🗓 Сводный отчёт за месяц:\n{report}")
+
+
+@router.message(F.text == "♾ Всё время")
+async def admin_summary_all(message: Message):
+    """отчёт за всё время"""
+    report = get_admin_summary("all")
+    await message.answer(f"♾ Сводный отчёт за всё время:\n{report}")
+
+
+@router.message(F.text == "⬅️ Назад")
+async def back_to_admin_menu(message: Message, state: FSMContext):
+    """возврат в админ-меню"""
+    await state.clear()
+    await message.answer(
+        "Админ-меню:",
+        reply_markup=reply_admin_menu()
+    )
