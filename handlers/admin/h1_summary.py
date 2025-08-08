4630f54643b6bebcacb2cb04587ebd34e25b23b0
Nick <idainswan@gmail.com>
2025-08-08 11:22:03 +0300

    в обработчик handle_summary_report интегрирована функция сводного отчета для админа

--- handlers/admin/h1_summary.py
+++ handlers/admin/h1_summary.py
@@ -1,16 +1,29 @@
 from aiogram import Router, F
 from aiogram.types import Message
 
+from services.google_sheets import get_full_report
+
 router = Router()
 
 @router.message(F.text == "📊 Сводный отчёт")
 async def handle_summary_report(message: Message):
-    """ отчет для администратора """
-    # fake_report - заглушка, далее заменю на gooogle sheets
-    fake_report = (
-        "📊 Сводный отчёт:\n"
-        "👤 Иванов — Доход: 4500₽, Расход: 1200₽\n"
-        "👤 Петров — Доход: 3200₽, Расход: 800₽\n"
-        "👤 Сидоров — Доход: 6100₽, Расход: 2000₽"
+    """отчёт для администратора"""
+    report = get_full_report()
+
+    total = report["total"]
+    text = (
+        f"📊 Сводный отчёт за всё время:\n"
+        f"Доход: {total['income']:.2f} ₽\n"
+        f"Расход: {total['expense']:.2f} ₽\n"
+        f"Разница: {total['diff']:.2f} ₽\n\n"
+        f"👤 По пользователям:\n"
     )
-    await message.answer(fake_report)
+
+    for username, stats in report["users"].items():
+        text += (
+            f"{username} — Доход: {stats['income']:.2f} ₽\n "
+            f"Расход: {stats['expense']:.2f} ₽\n "
+            f"Разница: {stats['diff']:.2f} ₽\n"
+        )
+
+    await message.answer(text)
